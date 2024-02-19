a22a6bca891011b0bbd91cccbf016420d9af0f2f
danikhan632 <danikhan632@gmail.com>
2024-02-18 20:43:44 -0500

    scaffolding

--- third_party/triton_shared/tools/triton-arm-opt/triton-arm-opt.cpp
+++ third_party/triton_shared/tools/triton-arm-opt/triton-arm-opt.cpp
@@ -36,98 +36,25 @@ struct GenericConversion : public OpRewritePattern<linalg::GenericOp> {
 
   LogicalResult matchAndRewrite(linalg::GenericOp op,
                                 PatternRewriter &rewriter) const override {
-    llvm::outs() << "Full GenericOp: ";
-    op.print(llvm::outs());
-    llvm::outs() << "\n\n";
 
-    auto indexingMaps = op.getIndexingMaps();
-    llvm::outs() << "Indexing Maps:\n";
-    for (auto map : indexingMaps) {
-      map.print(llvm::outs());
-      llvm::outs() << "\n";
-    }
-
-    auto iteratorTypes = op.getIteratorTypes();
-    llvm::outs() << "Iterator Types:\n";
-    for (auto type : iteratorTypes) {
-      llvm::outs() << type << "\n";
-    }
-
-    auto &region = op.getRegion();
-    llvm::outs() << "Region:\n";
-    for (auto &block : region) {
-      block.print(llvm::outs());
-      llvm::outs() << "\n";
-    }
 
     return success();
   }
 };
 } 
 
-namespace {
 
-struct ConvertArithAddfToArmSVE : public OpRewritePattern<arith::AddFOp> {
-  using OpRewritePattern<arith::AddFOp>::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(arith::AddFOp op,
-                                PatternRewriter &rewriter) const override {
-
-    llvm::outs() << "repl\n";
-
-    Value lhs = op.getOperand(0);
-    Value rhs = op.getOperand(1);
-
-    int vectorWidth = 16; 
-
-    auto vectorType = VectorType::get({vectorWidth}, rewriter.getBF16Type());
-
-    auto broadcastedLhs =rewriter.create<vector::BroadcastOp>(op.getLoc(), vectorType, lhs);
-    auto broadcastedRhs =rewriter.create<vector::BroadcastOp>(op.getLoc(), vectorType, rhs);
-
-    auto maskTrue = rewriter.create<arith::ConstantOp>(
-        op.getLoc(),
-        DenseIntElementsAttr::get(
-            VectorType::get({vectorWidth}, rewriter.getI1Type()), true));
-
-    auto svboolType =
-        VectorType::get({16}, rewriter.getI1Type()); 
-    auto maskSvbool = rewriter.create<arm_sve::ConvertToSvboolOp>(
-        op.getLoc(), svboolType, maskTrue);
-
-    auto armAddOp = rewriter.create<arm_sve::ScalableMaskedAddFOp>(
-        op.getLoc(),
-        vectorType,     
-        maskSvbool,     
-        broadcastedLhs, 
-        broadcastedRhs  
-    );
-
-    rewriter.replaceOp(op, {armAddOp.getResult()});
-
-    return success();
-  }
-};
-} 
 
 class ArmMatmulConversionPass
     : public PassWrapper<ArmMatmulConversionPass, OperationPass<func::FuncOp>> {
 public:
+
   void runOnOperation() override {
     auto func = getOperation();
     MLIRContext *context = func.getContext();
 
-    llvm::outs() << "Registered dialects in the current MLIRContext:\n";
-    for (auto *dialect : context->getLoadedDialects()) {
-      llvm::outs() << dialect->getNamespace()
-                   << "\n"; 
-    }
-
     RewritePatternSet patterns(context);
     patterns.add<GenericConversion>(context);
-    // patterns.add<ArmMatmulConversion>(context);
-    
-    // patterns.add<ConvertArithAddfToArmSVE>(context);
 
     (void)applyPatternsAndFoldGreedily(func, std::move(patterns));
   }
